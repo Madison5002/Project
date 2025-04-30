8e041e4b8b2bb4a611b68278e5ca35461438ebb4
Madison <madison.parker@students.fhu.edu>
2025-04-30 12:04:42 -0500

    Update

--- Project.py
+++ Project.py
@@ -2,65 +2,66 @@ import random
 
 print("--------Choose one of the five options: Rock, Paper, Scissors, Lizard, or Spock.--------\nThe winner is determined by the following rules: \nScissors cuts Paper\nPaper covers Rock\nRock crushes Lizard\nLizard poisons Spock\nSpock smashes Scissors\nScissors decapitates Lizard\nLizard eats Paper\nPaper disproves Spock\nSpock vaporizes Rock\nRock crushes Scissors\nIf both players choose the same option, it’s a tie")
 
+lose = 'You lose'
+win = 'You win'
 class Game():
     def __init__(self):
         self.choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
-
     def rock(self, opponent_choice):
             if opponent_choice == 'Spock':
-                return 'Rock has been vaporized. You lose.'
+                return f'Rock has been vaporized. {lose}.'
             elif opponent_choice == 'Paper':
-                return 'Rock has been covered. You lose.'
+                return f'Rock has been covered. {lose}.'
             elif opponent_choice in ['Lizard', 'Scissors']: 
-                return f'Rock has crushed {opponent_choice}. You win.'
+                return f'Rock has crushed {opponent_choice}. {win}.'
             else:
                 return 'Draw'
             
     def paper(self, opponent_choice):
             if opponent_choice == 'Scissors':
-                return 'Paper has been cut. You lose.'
+                return f'Paper has been cut. {lose}.'
             elif opponent_choice == 'Lizard':
-                return 'Paper has been eaten. You lose.'
+                return f'Paper has been eaten. {lose}.'
             elif opponent_choice == 'Spock':
-                return 'Paper has disproved Spock. You win.'
+                return f'Paper has disproved Spock. {win}.'
             elif opponent_choice == 'Rock':
-                return 'Paper has covered Rock. You win.'
+                return f'Paper has covered Rock. {win}.'
             else:
                 return 'Draw'
             
     def scissors(self, opponent_choice):
             if opponent_choice == 'Rock':
-                return 'Scissors have been crushed. You lose.'
+                return f'Scissors have been crushed. {lose}.'
             elif opponent_choice == 'Spock':
-                return 'Scissors have been smashed. You lose.'
+                return f'Scissors have been smashed. {lose}.'
             elif opponent_choice == 'Paper':
-                return 'Scissors have cut Paper. You win.'
+                return f'Scissors have cut Paper. {win}.'
             elif opponent_choice == 'Lizard':
-                return 'Scissors have decapitated Lizard. You win.'
+                return f'Scissors have decapitated Lizard. {win}.'
             else:
                 return 'Draw'
             
     def spock(self, opponent_choice):
             if opponent_choice == 'Paper':
-                return 'Spock has been disproven. You lose.'
+                return f'Spock has been disproven. {lose}.'
             elif opponent_choice == 'Lizard':
-                return 'Spock has been poisoned. You lose.'
+                return f'Spock has been poisoned. {lose}.'
             elif opponent_choice == 'Rock':
-                return 'Spock has vaporized Rock. You win.'
+                return f'Spock has vaporized Rock. {win}.'
             elif opponent_choice == 'Scissors':
-                return 'Spock has smashed Scissors. You win.'
+                return f'Spock has smashed Scissors. {win}.'
             else:
                 return 'Draw'
             
     def lizard(self, opponent_choice):
             if opponent_choice == 'Rock':
-                return 'Lizard has been crushed. You lose.'
+                return f'Lizard has been crushed. {lose}.'
             elif opponent_choice == 'Scissors':
-                return 'Lizard has been decapitated. You lose.'
+                return f'Lizard has been decapitated. {lose}.'
             elif opponent_choice == 'Paper':
-                return 'Lizard has eaten Paper. You win.'
+                return f'Lizard has eaten Paper. {win}.'
             elif opponent_choice == 'Spock':
-                return 'Lizard has poisoned Spock. You win.'
+                return f'Lizard has poisoned Spock. {win}.'
             else:
                 return 'Draw'
             
